a9d203af8202ff7fba5130101b04b7e408b4ab1c
Marion0401 <71750635+Marion0401@users.noreply.github.com>
2021-11-07 19:01:48 +0100

    Revert "porte 2 ok"
    This reverts commit f9525ac53b67366a239956f29923eeede592a51c.

--- Projet_escape/Assets/Scripts/ButtonKey1.cs
+++ Projet_escape/Assets/Scripts/ButtonKey1.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ButtonKey1 : MonoBehaviour
 {
     
-    
+    // Start is called before the first frame update
     public void Click()
     {
         VariablesGlobales.door1 = true;
