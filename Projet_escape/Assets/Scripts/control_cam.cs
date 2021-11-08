537a22d86635807b57be3bbffba5e26c4a4b7158
Hakim <71723927+HakimDuparcq@users.noreply.github.com>
2021-11-08 20:53:23 +0100

    fix tourner

--- Projet_escape/Assets/Scripts/control_cam.cs
+++ Projet_escape/Assets/Scripts/control_cam.cs
@@ -10,6 +10,9 @@ public class control_cam : MonoBehaviour
     public float offsety = 10;
     private Rigidbody mrigid;
     Vector3 destination;
+    private Vector3 lautre;
+    Vector3 Rotation;
+    public float sensibilite =4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +25,24 @@ public class control_cam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(targetTransform.position);
+        //Debug.Log(targetTransform.right * Input.GetAxis("Mouse Y") * 20);
+        if (Input.GetMouseButton(1))
+        {
+            Rotation.z += Input.GetAxis("Mouse Y") * sensibilite;
+            Rotation.y += Input.GetAxis("Mouse X") * sensibilite;
+        }
+   
+
+        Rotation.z = Mathf.Clamp(Rotation.z, -100f, 400f);
+        //lautre=lautre + targetTransform.right * Input.GetAxis("Mouse X") * 2;
+        targetTransform.rotation = Quaternion.Euler(0, Rotation.y, Rotation.z);
+
         //destination = targetTransform.position - (targetTransform.forward * offsetx - targetTransform.up * offsety);
         destination = targetTransform.position - (targetTransform.forward*PlayerPrefs.GetFloat("offsettx") - targetTransform.up* PlayerPrefs.GetFloat("offsetty")) ;
         //Debug.Log(destination);
         //mrigid.AddForce((-transform.position + destination) * movForce);
         mrigid.AddForce((-transform.position + destination) * PlayerPrefs.GetFloat("force_cam"));
+        transform.LookAt(targetTransform.position);
     }
 
 
